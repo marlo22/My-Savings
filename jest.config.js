89d95ef22d619b5b7b3bc38de1aafe1302a4a961
Marcin Lasecki <marcin.1993@poczta.fm>
2019-08-07 20:41:15 +0200

    Fixed NativeBase import problem in Jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 // https://www.freecodecamp.org/news/setting-up-jest-enzyme-for-testing-react-native-40393ca04145/
 
 module.exports = {
+  preset: "react-native",
   setupFilesAfterEnv: ['<rootDir>setup-tests.js'],
   preset: 'react-native',
   testEnvironment: "enzyme",
   testEnvironmentOptions: {
       enzymeAdapter: "react16"
-  }  
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!react-native|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|tcomb-form-native)"
+  ]
 };
